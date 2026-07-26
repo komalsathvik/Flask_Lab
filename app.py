b2d68c425b6add67d1bdb6be7c11a97f819d6850
Komal Sathvik <mksathvik03@gmail.com>
2026-07-26 23:22:22 +0530

    handling forms

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app=Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/handle_login",methods=["GET","POST"])
+def handle_login():
+    if request.method=="GET":
+        print(request.username)
+        return "<h1>entered into GET METHOD"
+    else:
+        return f"<h1> Welcome {request.form["username"]} </h1>"
 app.run(debug=True)
